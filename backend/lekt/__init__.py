69c565df174008e9a65dadd870c4aa8d03142043
Paweł Sacawa <psacawa@math.toronto.edu>
2020-12-04 16:37:40 -0500

    Fix: LektConfig, signals now recognized

---
+++ backend/lekt/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'lekt.apps.LektConfig' 
--- backend/lekt/apps.py
+++ backend/lekt/apps.py
@@ -3,12 +3,13 @@ from django.apps import AppConfig
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.info)
+logger.setLevel(logging.INFO)
 
 class LektConfig(AppConfig):
     name = "lekt"
 
     def ready(self):
+        import lekt.signals  
         logger.info(f"Application {self.name} ready.")
         #  TODO 03/12/20 psacawa: start should blocked until a set of data migrations has
         #  occurred
