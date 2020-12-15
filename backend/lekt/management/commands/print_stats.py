90862d34db1ba04d7945a413da3401281e341d74
Paweł Sacawa <psacawa@math.toronto.edu>
2020-12-15 06:13:42 -0500

    Add: print_stats command robust to schema change

--- backend/lekt/management/commands/print_stats.py
+++ backend/lekt/management/commands/print_stats.py
@@ -1,26 +1,17 @@
 from django.core.management.base import BaseCommand
+from django.apps import apps
 from tabulate import tabulate
-from lekt import models
 
 
 class Command(BaseCommand):
-    help = "Print statistics of lekt models."
+    help = """Print basic statistics on the number of instances of an application's models
+    present in the database. By default, statistics on lekt are printed."""
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('--app', default='lekt', type=str, 
+            help="App to show statistics for (default lekt)")
 
-    def handle(self, *args, **options):
-        models_to_print = [
-            models.Language,
-            models.Voice,
-            models.Phrase,
-            models.Word,
-            models.Annotation,
-            models.Subscription,
-            models.TrackedItem,
-            models.TrackedWord,
-            models.TrackedAnnotation,
-            models.UserProfile,
-        ]
-        data = [[m._meta.object_name, m.objects.count()] for m in models_to_print]
+    def handle(self, app, **options):
+        models = apps.get_app_config(app).get_models()
+        data = ([m._meta.object_name, m.objects.count()] for m in models)
         print(tabulate(data, headers=["model name", "count"]))
