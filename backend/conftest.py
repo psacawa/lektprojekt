168e9ebd11b93b6f7919963a09adfe995b77cc8a
Paweł Sacawa <pawelsacawa@gmail.com>
2021-01-10 23:51:26 -0500

    Infra: pytest hook to inhibit pytest without jq in $PATH

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import Dict
 
 import pytest
@@ -5,6 +6,15 @@ from assertpy import add_extension
 from django.contrib.auth.models import User
 from jq import jq as jq_base
 from rest_framework.test import APIClient
+
+
+def pytest_configure(*args):
+    """docstring for pytest_cmdline_parse"""
+    if not shutil.which("jq"):
+        pytest.exit(
+            "The JSON parsing utility jq is an external dependency of the lekt test "
+            "suite. It was not found in PATH. Aborting..."
+        )
 
 
 @pytest.fixture()
