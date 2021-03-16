2dd6bbcd6b6935287dc5db9da525bfd5f8c890cb
Paweł Sacawa <pawelsacawa@gmail.com>
2021-03-16 15:52:25 -0400

    Fix: Authorization token had lower precedence than session ...
    ... this led to authed views being unusable without the sessionid
    cookie, e.g. from curl

--- backend/main/settings/api.py
+++ backend/main/settings/api.py
@@ -1,8 +1,14 @@
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        #  "rest_framework.authentication.SessionAuthentication",
-        "main.authentication.SessionAuthentication",
+        #  token auth has to be first otherwise the lazy signup mechanism will take
+        #  precedence on the views it's activated on, and the user will be identified as
+        #  an anonymous new account if there is no sessionid cookie accompanying the
+        #  Authorization header
         "rest_framework.authentication.TokenAuthentication",
+        #  this is the same as rest_framework SessionAuthentication, but it does not have
+        #  the csrf check. It promotes the django HttpRequest user to the rest_framework
+        #  Request user
+        "main.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "lekt.pagination.NormalPageNumberPagination",
     "PAGE_SIZE": 25,
