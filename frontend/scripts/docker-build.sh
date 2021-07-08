f041712ed87e44b73b173a7eafc5336eb859308d
Paweł Sacawa <pawelsacawa@gmail.com>
2021-07-08 03:02:19 -0400

    Infra: Add docker build/push scripts

---
+++ frontend/scripts/docker-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "--ecr" ]]; then
+  registry=886185890074.dkr.ecr.us-east-2.amazonaws.com/
+fi
+
+docker build -t ${registry}lektprojekt-frontend-image .
---
+++ frontend/scripts/docker-push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+registry=886185890074.dkr.ecr.us-east-2.amazonaws.com/
+docker push ${registry}lektprojekt-frontend-image
