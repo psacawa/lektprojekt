215a1ac86daac1bbae8ddaf011aec884f9b87050
Paweł Sacawa <pawelsacawa@gmail.com>
2021-07-24 04:00:44 -0400

    Infra: Load env vars from .env.local in backenv dev as well

--- backend/manage.py
+++ backend/manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from typing import Dict, Text
+
+from dotenv import dotenv_values
 
 #  adding this import causes tracebacks to be rendered in colour
 if os.environ.get("DJANGO_ENV", None) == "development":
@@ -20,6 +23,12 @@ def main():
         ) from exc
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+    values = {
+        **dotenv_values(".env"),
+        **dotenv_values(".env.local"),
+    }
+    os.environ.update(values)  # type: ignore
+
     execute_from_command_line(sys.argv)
 
 
